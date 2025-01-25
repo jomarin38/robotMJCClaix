54db68c499abd912d05b940440be6398c809eeab
Syl20-38 <sylvain.jubier@laposte.net>
2025-01-25 14:59:07 +0100

    j ai cree un fichier .h juste les definition des pins. comme cela c est tout au meme endroit et on peut vérifier ce qui est deja utilise

---
+++ robotHolomone3Roues/pinsout.h
@@ -0,0 +1,26 @@
+
+
+//*******************************************************************************//
+// Association des entrées du L298N, aux sorties utilisées sur notre Arduino Uno //
+//*******************************************************************************//
+// moteur avant
+#define borneAvENA        11      // On associe la borne "ENA" du L298N à la pin D10 de l'arduino
+#define borneAvIN1        12      // On associe la borne "IN1" du L298N à la pin D9 de l'arduino
+#define borneAvIN2        10      // On associe la borne "IN2" du L298N à la pin D8 de l'arduino
+#define borneAvIN3        7       // On associe la borne "IN3" du L298N à la pin D7 de l'arduino
+#define borneAvIN4        8       // On associe la borne "IN4" du L298N à la pin D6 de l'arduino
+#define borneAvENB        9       // On associe la borne "ENB" du L298N à la pin D5 de l'arduino
+
+// moteur arriere
+#define borneArENA        5       // On associe la borne "ENA" du L298N à la pin D10 de l'arduino
+#define borneArIN1        18      // On associe la borne "IN1" du L298N à la pin D9 de l'arduino
+#define borneArIN2        17      // On associe la borne "IN2" du L298N à la pin D8 de l'arduino
+#define borneArIN3        16      // On associe la borne "IN3" du L298N à la pin D7 de l'arduino
+#define borneArIN4        15      // On associe la borne "IN4" du L298N à la pin D6 de l'arduino
+#define borneArENB        3      // On associe la borne "ENB" du L298N à la pin D5 de l'arduino
+
+//radio
+#define LEFT_RIGHT_IN_PIN     6
+#define UP_DOWN_IN_PIN        4
+#define LAT_LEFT_RIGHT_PIN    2
+#define SW5_PIN               13
